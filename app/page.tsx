4063f95eaf391a45950085aa58ec79ef9c90a4f9
Hafidz Alhadid Rahman <hafidzrahman559@gmail.com>
2024-12-19 21:17:06 +0700

    test

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
 'use client';
+import next from 'next';
 import {useState} from 'react'
 
 const basicKnowledges : {
@@ -6,7 +7,7 @@ const basicKnowledges : {
 } = {
   Tuna : ['B1', 'B4', 'B15', 'B19'],
   Hiu : ['B2', 'B4', 'B6', 'B14'],
-  Barakuda : ['B3', 'B11', 'B13', 'B14'],
+  Barakuda : ['B3', 'B13', 'B14'],
   ClownFish : ['B5', 'B16', 'B17'],
   Marlin : ['B4', 'B6', 'B7', 'B15', 'B19'],
   Sarden : ['B6', 'B15', 'B19'],
@@ -42,14 +43,20 @@ const questions : {
   'B21' : "Apakah ikan memiliki warna biru?"
 }
 
-const answers : any[] = [];
+const yesAnswers : string[] = [];
+const noAnswers : string[] = [];
+const answered : string[] = [];
+
 
 export default function Home() {
   const [questionNumber, setQuestionNumber] = useState<number>(1);
   const [result, setResult] = useState<any[]>([]);
   const [summary, setSummary] = useState<string | null>(null);
+  const [gotcha, setGotcha] = useState<string | null>(null);
 
   let question = "";
+
+  if (gotcha === null) {
 
   if (result.length === 0) {
     question = questions[`B${questionNumber}`];
@@ -59,13 +66,14 @@ export default function Home() {
     let scores = new Array(Object.keys(basicKnowledges).length).fill(0);
     let totalScores = 0;
     let ok = false;
-    for (const answer of answers) {
-      if (!ok) {let i = 0;
+    for (const answer of yesAnswers) {
+      if (!ok) {
+      let i = 0;
       for (const key in basicKnowledges) {
         if(basicKnowledges[key].includes(answer) && !ok) {
         ++scores[i];
         ++totalScores;
-        if (basicKnowledges[key].length === scores[i] && !ok) {
+        if (basicKnowledges[key].length === scores[i]) {
           ok = true;
           setSummary(`Dari ciri-ciri yang diberikan, jenis ikan yang dimaksud adalah : ${key}`);
           setResult([]);
@@ -92,37 +100,82 @@ export default function Home() {
     }
   }
   }
-
+}
 
   async function onClick(answer : string) {
+    if (gotcha === null) {
     const maxQuestionNumber = Object.keys(questions).length;
-    if (answer === "yes" && questionNumber !== maxQuestionNumber) {
-      answers.push(`B${questionNumber}`);
+    let nextQuestion : number | undefined = (questionNumber+1);
+    if (questionNumber !== maxQuestionNumber) {
+      answered.push(`B${questionNumber}`);
+      if (answer === "yes") {
+        yesAnswers.push(`B${questionNumber}`);
 
+        for (const key in basicKnowledges) {
+          let counter = 0;
+          for (const yesAnswer of yesAnswers) {
+            if (basicKnowledges[key][counter] === yesAnswer) {
+              ++counter;
+            }
+          }
+          if (counter === basicKnowledges[key].length) {
+            setGotcha(key)
+          }
+        }
+
+      } else {
+        noAnswers.push(`B${questionNumber}`)
+      }
+      nextQuestion = yesAnswers.length === 0 ? nextQuestion : parseInt(filter(Object.values(basicKnowledges))!)
+      // console.log(Object.values(basicKnowledges).filter(array1D => array1D.filter(data => yesAnswers.)).filter(array1D => array1D.filter(data => !noAnswers.includes(data))))
     } else if (questionNumber === maxQuestionNumber) {
-      if(answer === 'yes') answers.push(`B${questionNumber}`);
-      setResult(answers);
+      answered.push(`B${questionNumber}`);
+      if(answer === 'yes') yesAnswers.push(`B${questionNumber}`);
+      else noAnswers.push(`B${questionNumber}`)
+      setResult(yesAnswers);
     }
 
     if (questionNumber <= maxQuestionNumber) {
-      setQuestionNumber((prevEvent) => {
-        return ++prevEvent;
-      });
+      console.log(nextQuestion)
+      setQuestionNumber(nextQuestion!);
     }
-    
+  }
+  }
+
+  function filter(array2D : string[][]) : string | undefined {
+      for (const array1D of array2D) {
+        let ok = true;
+        for (const data of array1D) {
+          if (noAnswers.includes(data)) {
+            ok = false
+            break;
+          }
+        }
+        if (ok) {
+          let ok1 = 0;
+          for (const data of array1D) {
+            if (yesAnswers.includes(data)) {
+              ++ok1;
+            } else {
+              if (ok1 === yesAnswers.length) return data.split("B")[1];
+            }
+          }
+        }
+      }
   }
 
 
   return (
     <div className="w-full h-[100vh] flex justify-center items-center">
         <div className="w-[60%] h-96 rounded-lg shadow-2xl py-10 px-6 flex flex-col justify-between">
-          {summary === null && <p>{question}</p>}
-          {(result.length === 0 && summary === null) && <div className='flex justify-between items-center'>
+          {gotcha !== null && <p>{`Dari ciri-ciri yang diberikan, jenis ikan yang dimaksud adalah : ${gotcha}`}</p>}
+          {summary === null && gotcha === null && <p>{question}</p>}
+          {(result.length === 0 && summary === null && gotcha === null) && <div className='flex justify-between items-center'>
           <button onClick={() => onClick('yes')} >Ya</button>
           <button onClick={() => onClick('no')} >Tidak</button>
           </div>}
-          {result.length === 0 && summary !== null && <p>{summary}</p>}
-          {result.length !== 0 && summary !== null && 
+          {gotcha === null && result.length === 0 && summary !== null && <p>{summary}</p>}
+          {gotcha === null && result.length !== 0 && summary !== null && 
           <div>
             <p>{summary}</p>
             {result.map((data,i) => <p key={i}>{data}</p>)}
